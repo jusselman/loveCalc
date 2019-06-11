35a5469fc15c4125dc55afa78ba54b55799f8f8d
Joshua Usselman <usselmanjosh@gmail.com>
2019-06-10 23:31:29 -0700

    random nums

---
+++ js/main.js
@@ -0,0 +1,8 @@
+prompt('Who are you?');
+prompt('Who are they?');
+
+var heartPoints = Math.random() * 100;
+heartPoints = Math.floor(heartPoints) + 1;
+console.log(heartPoints);
+
+alert('Your heart points are ' + heartPoints + '%');
