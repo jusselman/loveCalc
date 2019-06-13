7e8c20ed50dc4f5d88937b7e3553dd754d6c43a5
Joshua Usselman <usselmanjosh@gmail.com>
2019-06-12 22:38:25 -0700

    fizzin

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
-prompt('Who are you?');
-prompt('Who are they?');
+function bmiCalculator(weight, height) {
+  var bmi = weight / (height * height);
+  if (bmi < 18.5) {
+    return 'Your BMI is ' + bmi + ', your underweight.';
+  } else if (bmi < 18.5 && bmi >= 24.9) {
+    return 'Your BMI is ' + bmi + ', your weight is average.';
+  } else if (bmi > 24.0) {
+    return 'Your BMI is ' + bmi + ' you fat fuck.';
+  } else {
+    return "I don't even know what to say";
+  }
+}
 
-var heartPoints = Math.random() * 100;
-heartPoints = Math.floor(heartPoints) + 1;
-console.log(heartPoints);
+// var weight = prompt('Please type your weight');
+// var height = prompt('Please type your height');
 
-alert('Your heart points are ' + heartPoints + '%');
+// function calcBMI(bmi) {
+
+// }
+
+bmiCalculator();
+
+// calcBMI();
